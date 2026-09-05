9d392d6699b43bad96e7e8aecd57add019a571c7
rafiadytiopratama19 <rafiadytiopratama19@gmail.com>
2026-09-05 17:04:15 +0700

    fix: set base path to /game/ for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Menjamin path asset berjalan lancar di GitHub Pages (subpath repo)
+  base: '/game/', // Sesuai dengan nama repository https://github.com/System-Modern/game
 });
